bb2e3f878d820477dbfeb096f873f476cbec3f7f
Nicolas Mora <nicolas2002mora@gmail.com>
2026-09-22 22:56:35 -0300

    optimizacion 2.2.2

--- mobile/src/screens/MisOportunidadesScreen.js
+++ mobile/src/screens/MisOportunidadesScreen.js
@@ -327,7 +327,7 @@ export default function MisOportunidadesScreen({
                       }}
                     >
                       <Text style={styles.btnSecondaryText}>
-                        Cerrar Incripciones
+                        Cerrar Inscripciones
                       </Text>
                     </TouchableOpacity>
 
